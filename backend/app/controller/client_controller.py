bfea877557d64ba33a795041e9b447e355e1967d
Turquoise-stack <murathan.aktimur2@gmail.com>
2025-01-06 18:45:16 +0100

    fix(backend): login-registry endpoints fixed with their hashing

--- backend/app/controller/client_controller.py
+++ backend/app/controller/client_controller.py
@@ -9,7 +9,7 @@ from sqlalchemy.sql import text
 
 router = APIRouter()
 
-# Dependency to get a database session
+# get db session
 def get_db():
     db = SessionLocal()
     try:
@@ -19,14 +19,15 @@ def get_db():
 
 @router.post("/register")
 def register(request: RegisterRequest, db: Session = Depends(get_db)):
-    # Check if user already exists
+    # validate user if exists
     existing_user = db.query(User).filter(User.email == request.email).first()
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already registered")
 
     # Hash the password
     hashed_password = get_password_hash(request.password)
-    new_user = User(email=request.email, password=hashed_password)
+
+    new_user = User(email=request.email, hashed_password=hashed_password)
 
     db.add(new_user)
     db.commit()
@@ -36,17 +37,20 @@ def register(request: RegisterRequest, db: Session = Depends(get_db)):
 
 @router.post("/login")
 def login(request: LoginRequest, db: Session = Depends(get_db)):
+    # query the user by email
     user = db.query(User).filter(User.email == request.email).first()
-    if not user or not verify_password(request.password, user.password):
+
+    # Validatte user and password
+    if not user or not verify_password(request.password, user.hashed_password):
         raise HTTPException(status_code=400, detail="Invalid credentials")
     
+    # Create a jwt token
     token = create_access_token({"sub": user.email})
     return {"access_token": token, "token_type": "bearer"}
 
 @router.get("/health-check")
 def health_check(db: Session = Depends(get_db)):
     try:
-        # Use text to execute the SQL query
         db.execute(text("SELECT 1"))
         return {"status": "ok", "database": "connected"}
     except Exception as e:
