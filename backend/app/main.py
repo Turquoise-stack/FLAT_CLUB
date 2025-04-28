82510c61e253ca07c0438b791b9def20f73aba1b
Turquoise-stack <murathan.aktimur2@gmail.com>
2025-04-28 19:14:28 +0200

    fix(main): backend fixed for cors error

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,24 +1,32 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import os
+
 from controller.client_controller import router as client_router
 from controller.system_controller import router as system_router
 from controller.listing_controller import router as listing_router
 from controller.message_controller import router as message_router
 from fastapi.middleware.cors import CORSMiddleware
 
-
 app = FastAPI()
 
+# Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow requests from all origins. Replace "*" with specific origins for production.
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],  
+    allow_headers=["*"],
 )
 
-# routers
+# Serve static files
+if not os.path.exists("uploads"):
+    os.makedirs("uploads")
+
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+
+# Routers
 app.include_router(client_router, prefix="/api", tags=["client"])
 app.include_router(system_router, prefix="/api", tags=["system"])
 app.include_router(listing_router, prefix="/api", tags=["listing"])
 app.include_router(message_router, prefix="/api", tags=["message"])
-
