4b81e893aee1a5e02ac494a113c8c4b52ae98665
Turquoise-stack <murathan.aktimur2@gmail.com>
2025-06-15 02:45:33 +0200

    react-link update-2

--- my-project/src/pages/Login.tsx
+++ my-project/src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Box, Typography, TextField, Button } from "@mui/material";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import api from '../api/api'  
 
 const Login = () => {
